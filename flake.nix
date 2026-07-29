ef716e8bb503e3234e3a079ae07d5e42d394d0ae
Khai Phung <phunga.prod@gmail.com>
2026-07-29 16:51:32 -0400

    update nix package

--- flake.nix
+++ flake.nix
@@ -1,19 +1,23 @@
 {
-  description = "A minimal flake that runs the Nixpkgs hello package";
+  description = "A simple hello/cowsay runnable flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
-      helloPkg = pkgs.hello;
-    in {
-      packages.${system}.default = helloPkg;
-
-      apps.${system}.default = {
-        type = "app";
-        program = "${helloPkg}/bin/hello";
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.writeShellApplication {
+          name = "greet";
+          runtimeInputs = [ pkgs.cowsay ];
+          text = ''
+            cowsay "Hello from the range! Activated at $(date)"
+          '';
+        };
+      });
 }
